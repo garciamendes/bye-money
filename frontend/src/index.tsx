331c0bbd7f4e85b7c16e45322eed7e12893286cd
Aron Matheus Garcia Mendes <mmendes@morphus.com.br>
2021-12-25 20:23:28 -0300

    Base project

---
+++ frontend/src/index.tsx
@@ -0,0 +1,14 @@
+// React
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Local
+import './styles/index.scss'
+import { App } from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
